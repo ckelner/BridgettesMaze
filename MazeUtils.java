a62f3ffca9682a57842f7ab4ff479a37cc3d7ee9
ckelner <chris.kelner@gmail.com>
2015-12-11 00:19:16 -0500

    several enhancements and fixes
    prevent user from moving out of bounds (Closes #3)
    don’t print menu twice (Closes #2)
    fix menu looping
    move maze to its own function

--- MazeUtils.java
+++ MazeUtils.java
@@ -6,14 +6,9 @@ public class MazeUtils {
     Scanner input = new Scanner(System.in);
     int x=7;
     int y=1;
-    int userChoice;
-
-    //Makes the Menu close after entering a number. Opens again after the user solves the maze
-    boolean quitMenu = false;
-
+    int mazeChoice;
     //Makes the Maze and the menu to stop appearing. (Stops Program)
     boolean quitSolvedOrQ = false;
-
     //3D Array with three separate mazes
     int maze[][][] = {
       {
@@ -49,98 +44,121 @@ public class MazeUtils {
     };
 
     //Menu
+    System.out.println("==========================");
+    System.out.println("= Welcome to Maze Mania! =");
+    System.out.println("==========================");
     do {
-      System.out.println("Welcome to Maze Mania! To play a maze, enter a number: 1, 2, or 3.");
-      System.out.println("Maze number correlates to it's level of difficulty, so choose accordingly.");
+      quitSolvedOrQ = false;
+      mazeChoice = displayMazeMenu(input);
 
-      userChoice = input.nextInt();
+      if(mazeChoice != -1) {
+        //Gets directions to move in maze and implements them
+        do {
+          displayMaze(maze,x,y,mazeChoice);
+          System.out.println("Enter a command: (l)eft, (r)ight, (u)p, (d)own, (q)uit");
+          String commands = input.next();
 
-      if (userChoice < 1 || userChoice > 3 )
+          for(int i= 0; i<commands.length(); i++) {
+            System.out.println();
+            // Left
+            if(commands.charAt(i) == 'l')
+            {
+              if(y-1 >= 0 && maze[mazeChoice][x][y-1] != 1)
+              {
+                y -= 1;
+              }
+              else
+              {
+                System.out.println("You ran into a wall! Please enter a vaild command");
+              }
+            }
+            //Right
+            if(commands.charAt(i) == 'r')
+            {
+              if(y+1 < 8 && maze[mazeChoice][x][y+1] != 1)
+              {
+                y += 1;
+              }
+              else
+              {
+                System.out.println("You ran into a wall! Please enter a vaild command");
+              }
+            }
+            //Up
+            if(x-1 >= 0 && commands.charAt(i) == 'u')
+            {
+              if(maze[mazeChoice][x-1][y] != 1)
+              {
+                x -= 1;
+              }
+              else
+              {
+                System.out.println("You ran into a wall! Please enter a vaild command");
+              }
+            }
+            //Down
+            if(x+1 < 8 && commands.charAt(i) == 'd')
+            {
+              if(maze[mazeChoice][x+1][y] != 1)
+              {
+                x += 1;
+              }
+              else
+              {
+                System.out.println("You ran into a wall! Please enter a vaild command");
+              }
+            }
+            //Quit
+            if(commands.charAt(i) == 'q')
+            {
+              quitSolvedOrQ = true;
+              break;
+            }
+            //Solved the maze
+            if(maze[mazeChoice][x][y] == 3)
+            {
+              System.out.println("Congralations, you solved the maze! Which one do you want to try next?");
+              // reset player starting position
+              // need to tie this to different spot for different mazes eventually
+              x=7;
+              y=1;
+              quitSolvedOrQ = true;
+              break;
+            }
+          }
+        } while(quitSolvedOrQ == false);
+      }
+    } while(true);
+  }
+
+  public static int displayMazeMenu(Scanner input){
+    System.out.println("To play a maze, enter a number: 1, 2, or 3.");
+    System.out.println("Maze number correlates to it's level of difficulty, so choose accordingly.");
+    System.out.println("Enter 'q' to quit.");
+    // must be string so user can quit from main menu
+    String userChoice = input.next();
+
+    if(userChoice.equals("1") || userChoice.equals("2") || userChoice.equals("3") || userChoice.equals("q"))
+    {
+      if(userChoice.equals("q"))
       {
-        System.out.println("Please enter a 1, 2, or 3");
+        System.exit(0);
       }
       else
       {
+        // convert user choice from string to int for use in getting maze from array
+        int mazeChoice = Integer.parseInt(userChoice);
         // arrays are zero based, so minus 1 to select the right maze
-        userChoice--;
-        quitMenu = true;
+        mazeChoice -= 1;
+        return mazeChoice;
       }
-    } while(quitMenu == false);
-
-    //Gets directions to move in maze and implements them
-    do {
-      displayMaze(maze,x,y,userChoice);
-      System.out.println("Enter a command: (l)eft, (r)ight, (u)p, (d)own, (q)uit");
-      String commands = input.next();
-
-      for(int i= 0; i<commands.length(); i++) {
-        System.out.println();
-        // Left
-        if(commands.charAt(i) == 'l')
-        {
-          if(y-1 >= 0 && maze[userChoice][x][y-1] != 1)
-          {
-            y -= 1;
-          }
-          else
-          {
-            System.out.println("You ran into a wall! Please enter a vaild command");
-          }
-        }
-        //Right
-        if(commands.charAt(i) == 'r')
-        {
-          if(y+1 < 8 && maze[userChoice][x][y+1] != 1)
-          {
-            y += 1;
-          }
-          else
-          {
-            System.out.println("You ran into a wall! Please enter a vaild command");
-          }
-        }
-        //Up
-        if(x-1 >= 0 && commands.charAt(i) == 'u')
-        {
-          if(maze[userChoice][x-1][y] != 1)
-          {
-            x -= 1;
-          }
-          else
-          {
-            System.out.println("You ran into a wall! Please enter a vaild command");
-          }
-        }
-        //Down
-        if(x+1 < 8 && commands.charAt(i) == 'd')
-        {
-          if(maze[userChoice][x+1][y] != 1)
-          {
-            x += 1;
-          }
-          else
-          {
-            System.out.println("You ran into a wall! Please enter a vaild command");
-          }
-        }
-        //Quit
-        if(commands.charAt(i) == 'q')
-        {
-          quitSolvedOrQ = true;
-          quitMenu = false;
-          break;
-        }
-        //Solved the maze
-        if(maze[userChoice][x][y] == 3)
-        {
-          System.out.println("Congralations, you solved the maze! Which one do you want to try next?");
-          userChoice = input.nextInt();
-          quitSolvedOrQ = true;
-          quitMenu = false;
-          break;
-        }
-      }
-    } while(quitSolvedOrQ == false);
+    }
+    else
+    {
+      System.out.println("Please enter a maze choice: 1, 2, or 3, or enter 'q' to quit.");
+      return -1;
+    }
+    return -1;
   }
 
   // Displays the maze correctly
